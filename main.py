6e1d45d29eab1f97b6a8fe47dcb74be61930a605
Dr-Kaisa <121852722+Dr-Kaisa@users.noreply.github.com>
2023-11-24 19:39:20 +0800

    main.py
    - 增加了系统稳定性
    - 修复了一些bug

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 
-
 def getAccount():
     file = open("account.txt")
     account = file.readline()
@@ -10,18 +9,24 @@ def getAccount():
     file.close()
     return account, password
 
-
+# 初始化浏览器
 def ini():
+
+
     driver = webdriver.Edge()
     driver.maximize_window()
     driver.get(
         "https://passport.zhihuishu.com/login?service=https://onlineservice-api.zhihuishu.com/gateway/f/v1/login/gologin")
     return driver
 
-
+# 登录
 def login(driver, args):
     driver.find_element(By.ID, "lUsername").send_keys(args[0])
     driver.find_element(By.ID, "lPassword").send_keys(args[1])
+    try:
+        driver.find_element(By.CLASS_NAME, "wall-sub-btn").click()
+    except:
+        pass
 
 
 def closeGarbageMessage():
@@ -60,6 +65,7 @@ def closeTheAlert(driver):
     print('点击关闭')
 
 
+
 if __name__ == '__main__':
 
     # 初始化浏览器
@@ -80,13 +86,24 @@ if __name__ == '__main__':
             print("已进入播放页面")
             closeGarbageMessage()
             print("关闭垃圾弹窗，即将开始播放\n")
+
+            sleep(3)
+            try:
+                closeTheAlert(driver)
+            except:
+                pass
+
             Allvideolist = getAllTheCurseLIst()
             print("已保存全部课程列表")
             lastFinishedCurse = getTheFinishedCurse()
             print(f'之前已完成课程数量：{len(lastFinishedCurse)}')
+
+            # 进入未完成课程页面
             Allvideolist[len(lastFinishedCurse)].click()
-            print('初始化课程列表成功，进入循环')
-            sleep(5)
+            sleep(0.5)
+            Allvideolist[len(lastFinishedCurse)].click()
+            sleep(0.5)
+            Allvideolist[len(lastFinishedCurse)].click()
             # 开始播放
             while True:
                 sleep(1)
@@ -95,22 +112,40 @@ if __name__ == '__main__':
                     closeTheAlert(driver)
                 except:
                     print("关闭弹窗失败")
+                # 播放视频，防止其他原因暂停播放
                 try:
-                    # 播放视频，防止其他原因暂停播放
+
                     unPause(driver)
                 except:
                     pass
 
                 sleep(1)
-                # 通存放现在已完成的课程的列表
-                nowFinishCurse = getTheFinishedCurse()
-                print(f'全部课程数：{len(Allvideolist)}')
-                print(f'之前已完成课程数：{len(lastFinishedCurse)}')
-                print(f'现在已完成课程数：{len(nowFinishCurse)}')
+                # 存放现在已完成的课程的列表
+                try:
+                    nowFinishedCurse = getTheFinishedCurse()
+                except:
+                    nowFinishedCurse = getTheFinishedCurse()
 
+                # 输出播放信息
+                print("-------------------------------------------------------")
+                print(f'全部课程数：     {len(Allvideolist)}')
+                print(f'之前已完成课程数：{len(lastFinishedCurse)}')
+                print(f'现在已完成课程数：{len(nowFinishedCurse)}')
+                try:
+                    tt = driver.find_element(By.CSS_SELECTOR, ".current_play>.cataloguediv-c>div>div span").text
+                    print(f"当前视频进度：   {tt}")
+                except:
+                    pass
                 # 当现在已完成比之前已完成多一个，自动播放下一个视频
-                if len(nowFinishCurse) == len(lastFinishedCurse) + 1:
-                    lastFinishedCurse = nowFinishCurse
-                    Allvideolist[len(lastFinishedCurse)].click()
-                    sleep(5)
+                if len(nowFinishedCurse) == len(lastFinishedCurse) + 1:
+                    Allvideolist[len(nowFinishedCurse)].click()
+                    Allvideolist[len(nowFinishedCurse)].click()
+                    sleep(1)
+                    Allvideolist[len(nowFinishedCurse)].click()
+                    Allvideolist[len(nowFinishedCurse)].click()
+                    sleep(1)
+                    Allvideolist[len(nowFinishedCurse)].click()
+                    Allvideolist[len(nowFinishedCurse)].click()
+                    lastFinishedCurse = nowFinishedCurse
+
         sleep(2)
