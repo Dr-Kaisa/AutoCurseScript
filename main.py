de666448ec1f99fc84bff02da85cfd786a50aadb
Dr-Kaisa <121852722+Dr-Kaisa@users.noreply.github.com>
2023-12-08 10:18:31 +0800

    Update main.py
    - 添加了没什么卵用但是很帅的东西

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
+from tqdm import tqdm
+
 
 def getAccount():
     file = open("account.txt")
@@ -9,15 +11,15 @@ def getAccount():
     file.close()
     return account, password
 
+
 # 初始化浏览器
 def ini():
-
-
     driver = webdriver.Edge()
     driver.maximize_window()
     driver.get(
         "https://passport.zhihuishu.com/login?service=https://onlineservice-api.zhihuishu.com/gateway/f/v1/login/gologin")
     return driver
+
 
 # 登录
 def login(driver, args):
@@ -59,10 +61,8 @@ def unPause(driver):
 
 def closeTheAlert(driver):
     driver.find_element(By.CSS_SELECTOR, ".el-scrollbar__view>.topic>.radio>.topic-list>.topic-item svg").click()
-    print('点击选项')
     sleep(0.5)
     driver.find_element(By.CSS_SELECTOR, "#outContainer>.el-dialog__wrapper>.el-dialog>.el-dialog__footer span").click()
-    print('点击关闭')
 
 
 
@@ -71,6 +71,7 @@ if __name__ == '__main__':
     # 初始化浏览器
     print("正在初始化浏览器")
     driver = ini()
+
 
     # 获取账号密码并登录
     while True:
@@ -84,8 +85,14 @@ if __name__ == '__main__':
         print("等待进入播放页面")
         if driver.find_elements(By.CLASS_NAME, "clearfix.video"):
             print("已进入播放页面")
+            progressBar = tqdm(total=100,desc='当前视频进度',bar_format='{l_bar}{bar}|')
+            progressBar.clear()
             sleep(2)
             closeGarbageMessage()
+            try:
+                closeTheAlert(driver)
+            except:
+                pass
             print("关闭垃圾弹窗，即将开始播放\n")
 
             sleep(3)
@@ -98,7 +105,7 @@ if __name__ == '__main__':
             print(f'全部课程数：     {len(Allvideolist)}')
             lastFinishedCurse = getTheFinishedCurse()
             print(f'之前已完成课程数：{len(lastFinishedCurse)}')
-
+            print("-------------------------------------------------------")
             # 进入未完成课程页面
             Allvideolist[len(lastFinishedCurse)].click()
             sleep(0.5)
@@ -107,12 +114,14 @@ if __name__ == '__main__':
             Allvideolist[len(lastFinishedCurse)].click()
             # 开始播放
             while True:
+
+
                 sleep(1)
                 try:
-                    print("正在关闭弹窗")
+
                     closeTheAlert(driver)
                 except:
-                    print("关闭弹窗失败")
+                    pass
                 # 播放视频，防止其他原因暂停播放
                 try:
 
@@ -128,13 +137,13 @@ if __name__ == '__main__':
                     nowFinishedCurse = getTheFinishedCurse()
 
                 # 输出播放信息
-                print("-------------------------------------------------------")
-                print(f'全部课程数：     {len(Allvideolist)}')
-                print(f'之前已完成课程数：{len(lastFinishedCurse)}')
-                print(f'现在已完成课程数：{len(nowFinishedCurse)}')
                 try:
-                    tt = driver.find_element(By.CSS_SELECTOR, ".current_play>.cataloguediv-c>div>div span").text
-                    print(f"当前视频进度：    {tt}")
+                    tt = driver.find_element(By.CSS_SELECTOR, ".current_play>div>div span").text
+                    progressBar.reset()
+                    progressBar.update(float(tt.strip(' % ')))
+                    progressBar.display()
+
+
                 except:
                     pass
                 # 当现在已完成比之前已完成多一个，自动播放下一个视频
@@ -142,11 +151,8 @@ if __name__ == '__main__':
                     sleep(10)
                     Allvideolist[len(nowFinishedCurse)].click()
                     sleep(2)
-                    print(f"正在播放：{driver.find_element(By.CLASS_NAME, "current_play")}")
-                    print(f"要播放的：{Allvideolist[len(nowFinishedCurse)]}")
                     # 检测视频是否切换成功
-                    if driver.find_element(By.CLASS_NAME, "current_play")==Allvideolist[len(nowFinishedCurse)]:
+                    if driver.find_element(By.CLASS_NAME, "current_play") == Allvideolist[len(nowFinishedCurse)]:
                         lastFinishedCurse = nowFinishedCurse
-
 
         sleep(2)
